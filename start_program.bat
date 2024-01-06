4190d31ec05902ea01e1d17a62e8df4b6f07bab0
ThePoolboy <ethanpool1@gmail.com>
2024-01-05 19:47:12 -0600

    added start_program details

--- start_program.bat
+++ start_program.bat
@@ -1,5 +1,3 @@
 @ECHO OFF
-
-PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~install_mods.ps1""' -Verb RunAs}"
-
+PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""C:\lfc_manager-poolec\lfc_manager-poolec\install_mods.ps1""' -Verb RunAs}"
 PAUSE
